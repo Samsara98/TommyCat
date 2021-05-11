6071740ccd78abd8e7028aab75a5af9a1063131f
samsara98 <wk1044481101@163.com>
2021-05-11 15:24:26 +0800

    A

--- src/JerryRat.java
+++ src/JerryRat.java
@@ -1,20 +1,65 @@
-import java.io.IOException;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Locale;
 
 public class JerryRat implements Runnable {
+
+    public static final String SERVER_PORT = "8080";
+    public static final String WEB_ROOT = "res/webroot";
+    ServerSocket serverSocket;
+
+    public JerryRat() throws IOException {
+        serverSocket = new ServerSocket(Integer.parseInt(SERVER_PORT));
+    }
+
     @Override
-    public void run(){
+    public void run() {
         System.out.println("I'm Jerry the rat!");
-        EchoServer echoServer = null;
+        try (
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            String request = in.readLine();
+            while (request != null) {
+                String entityBody;
+                String[] req = request.split(" ");
+                String requestMethod = req[0];
+                if (!requestMethod.toLowerCase(Locale.ROOT).equals("get")) {
+                    request = in.readLine();
+                    continue;
+                }
+                String requestPath = req[1];
+                File requestFile = new File(WEB_ROOT + requestPath);
+                if (requestFile.isDirectory()) {
+                    requestFile = new File(requestFile.getAbsolutePath() + "/index.html");
+                    FileReader fos = new FileReader(requestFile);
+                    char[] content = new char[(int) requestFile.length()];
+                    fos.read(content);
+                    entityBody = String.valueOf(content);
+                    out.println(entityBody);
+                } else {
+                    FileReader fos = new FileReader(requestFile);
+                    char[] content = new char[(int) requestFile.length()];
+                    fos.read(content);
+                    entityBody = String.valueOf(content);
+                    out.println(entityBody);
+                }
+                request = in.readLine();
+            }
+        } catch (IOException e) {
+            System.err.println("TCP连接错误！");
+        }
+    }
+
+    public static void main(String[] args) {
+        JerryRat jerryRat = null;
         try {
-            echoServer = new EchoServer();
+            jerryRat = new JerryRat();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        new Thread(echoServer).run();
-    }
-
-    public static void main(String[] args){
-        JerryRat jerryRat = new JerryRat();
-        jerryRat.run();
+        new Thread(jerryRat).start();
     }
 }
