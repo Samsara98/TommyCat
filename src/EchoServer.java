ab9a93947de96c65a3ad114306c9e929f211cfc4
samsara98 <wk1044481101@163.com>
2021-05-11 11:51:37 +0800

    A

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,12 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
 
 public class EchoServer implements Runnable {
-    public static final String SERVER_PORT = "3456";
+    public static final String SERVER_PORT = "8080";
+    public static final String WEBROOT = "res/webroot";
     ServerSocket serverSocket;
 
     public EchoServer() throws IOException {
@@ -20,14 +20,35 @@ public class EchoServer implements Runnable {
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         ) {
-            String line = in.readLine();
-            int i = 1;
-            while (line != null) {
-                out.println(i + ". " + line);
-                i++;
-                line = in.readLine();
+            String request = in.readLine();
+            while (request != null) {
+                String entityBody = "";
+                String[] req = request.split(" ");
+                String requestMethod = req[0];
+                if (!requestMethod.equals("GET") || req.length < 2) {
+                    System.err.println("错误的请求方法！");
+                    break;
+                }
+                String requestPath = req[1];
+                File requestFile = new File(WEBROOT + requestPath);
+                if (requestFile.isDirectory()) {
+                    requestFile = new File(WEBROOT + requestPath + "/index.html");
+                    FileReader fos = new FileReader(requestFile);
+                    char[] content = new char[(int) requestFile.length()];
+                    fos.read(content);
+                    entityBody = String.valueOf(content);
+                    out.println(entityBody);
+                } else {
+                    FileReader fos = new FileReader(requestFile);
+                    char[] content = new char[(int) requestFile.length()];
+                    fos.read(content);
+                    entityBody = String.valueOf(content);
+                    out.println(entityBody);
+                }
+                request = in.readLine();
             }
         } catch (IOException e) {
+            e.printStackTrace();
             System.err.println("TCP连接错误！");
         }
     }
